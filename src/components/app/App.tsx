169fc4df52e50cab72996afe7afc5df6c2cf11cf
Pavel <krobynetspavel@gmail.com>
2024-09-02 18:52:21 +0300

    first commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import Interface from "../interface/Interface";
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Interface />
+    </div>
+  );
+}
---
+++ src/components/interface/Interface.tsx
@@ -0,0 +1,8 @@
+import "./interface.scss"
+
+export default function Interface() { 
+	return (
+		<div className="interface">
+		</div>
+	);
+}
