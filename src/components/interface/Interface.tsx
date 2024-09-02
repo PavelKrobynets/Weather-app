1901feaf3b97a1b3732968946be4878991d2435f
Pavel <krobynetspavel@gmail.com>
2024-09-02 20:47:04 +0300

    almost created interface

--- src/components/interface/Interface.tsx
+++ src/components/interface/Interface.tsx
@@ -1,8 +1,40 @@
-import "./interface.scss"
+import "./interface.scss";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import icon from "../../assets/cloudy.png";
+import { WiHumidity } from "react-icons/wi";
+import { LuWind } from "react-icons/lu";
 
-export default function Interface() { 
-	return (
-		<div className="interface">
-		</div>
-	);
+export default function Interface() {
+  return (
+    <div className="interface">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          className="search-bar-input"
+        />
+        <button className="search-bar-button">
+          <FaMagnifyingGlass />
+        </button>
+      </div>
+      <h2>Uzhhorod</h2>
+      <img src={icon} alt="weather-icon" className="interface__icon" />
+      <div className="interface__temperature">
+        <span className="interface__temperature-value">22°C</span>
+        <span className="interface__temperature-clouds">Clouds</span>
+      </div>
+      <div className="interface__info">
+        <div className="interface__info-humidity">
+          <WiHumidity/>
+          <span className="interface__info-humidity-value">60%</span>
+          <span className="interface__info-humidity-label">Humidity</span>
+        </div>
+        <div className="interface__info-speed">
+				<LuWind />
+          <span className="interface__info-speed-value">60km/h</span>
+          <span className="interface__info-speed-label">Speed</span>
+				</div>
+      </div>
+    </div>
+  );
 }
